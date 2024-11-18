364479b56e99b4746b08540608f06c877479b29c
Dennis Zhang <dennis.zhang@parks.nyc.gov>
2024-11-18 16:39:59 -0500

    add postDomainValues()

--- eComply.py
+++ eComply.py
@@ -3,9 +3,9 @@ import urllib.parse
 
 
 class eComply:
-    url: str
-    credential: dict
-    headers: dict = {
+    _url: str
+    _credential: dict
+    _headers: dict = {
         "Authorization": "",
         "Content-Type": "application/json",
         "Accept": "application/json",
@@ -17,16 +17,16 @@ class eComply:
         username: str,
         password: str,
     ) -> None:
-        self.url = url
-        self.credential = {"username": username, "password": password}
+        self._url = url
+        self._credential = {"username": username, "password": password}
 
     def getAPIToken(self) -> str:
         url = (
-            self.url
+            self._url
             + "Authentication/ValidateUser?"
-            + urllib.parse.urlencode(self.credential)
+            + urllib.parse.urlencode(self._credential)
         )
-        response = requests.post(url, headers=self.headers)
+        response = requests.post(url, headers=self._headers)
 
         if response.ok:
             result = response.json()
@@ -37,22 +37,43 @@ class eComply:
             return ""
 
     def verifyToken(self, token: str):
-        url = self.url + "Authentication/AuthenticateToken?token=" + token
+        url = self._url + "Authentication/AuthenticateToken?token=" + token
         print(url)
-        response = requests.post(url, headers=self.headers)
+        response = requests.post(url, headers=self._headers)
         result = response.json()
         print(result)
 
     def getContracts(self):
         token = self.getAPIToken()
         # print(self.verifyToken(token))
-        self.headers["Authorization"] = "Bearer " + token
-        url = self.url + "Contracts/ExportContracts"
-        #         print(url)
-        response = requests.get(url, headers=self.headers)
+        self._headers["Authorization"] = "Bearer " + token
+        url = self._url + "Contracts/ExportContracts"
+        # print(url)
+        response = requests.get(url, headers=self._headers)
 
         if response.ok:
             result = response.json()
             return result["data"]
         else:
+            print(response.json())
+
+    def postDomainValues(self, domain):
+        token = self.getAPIToken()
+        self._headers["Authorization"] = "Bearer " + token
+        url = self._url + "Catalog/ImportDomainNames"
+
+        data = []
+        for value in domain["codedValues"]:
+            data.append(
+                {
+                    "domainName": domain["name"],
+                    "code": str(value["code"]),
+                    "value": value["name"],
+                }
+            )
+
+        response = requests.post(url, headers=self._headers, json=data)
+        print(response.json())
+        if response.ok:
+            response = requests.get(url, headers=self._headers)
             print(response.json())
