fce36710d7d20c81145bfd972b03d108b268068d
Dennis Zhang <dennis.zhang@parks.nyc.gov>
2024-11-19 16:17:44 -0500

    add getEntities() and postEntities() comment out verifyToken()

--- eComply.py
+++ eComply.py
@@ -1,4 +1,3 @@
-import re
 import requests
 import urllib.parse
 
@@ -21,7 +20,7 @@ class eComply:
         self._baseURL = baseURL
         self._credential = {"username": username, "password": password}
 
-    def getAPIToken(self) -> str:
+    def __getAPIToken(self) -> str:
         url = (
             self._baseURL
             + "Authentication/ValidateUser?"
@@ -38,21 +37,13 @@ class eComply:
             print(response.json())
             return ""
 
-    def getHeaders(self) -> dict:
-        token = self.getAPIToken()
+    def __getHeaders(self) -> dict:
+        token = self.__getAPIToken()
         self._headers["Authorization"] = "Bearer " + token
         return self._headers
 
-    def verifyToken(self, token: str):
-        url = self._baseURL + "Authentication/AuthenticateToken?token=" + token
-        print(url)
-        response = requests.post(url, headers=self._headers)
-        result = response.json()
-        print(result)
-
-    def getContracts(self) -> list:
-        url = self._baseURL + "Contracts/ExportContracts"
-        response = requests.get(url, headers=self.getHeaders())
+    def __getEntities(self, url: str) -> list:
+        response = requests.get(url, headers=self.__getHeaders())
 
         if response.ok:
             result = response.json()
@@ -61,9 +52,8 @@ class eComply:
             print(response.json())
             return []
 
-    def postContracts(self, contracts: list) -> bool:
-        url = self._baseURL + "Contracts/ImportContracts"
-        response = requests.post(url, headers=self.getHeaders(), json=contracts)
+    def __postEntities(self, url: str, entities: list) -> bool:
+        response = requests.post(url, headers=self.__getHeaders(), json=entities)
 
         if not response.ok:
             raise Exception(response)
@@ -94,6 +84,21 @@ class eComply:
             )
 
         return result["success"]
+
+    # def __verifyToken(self, token: str):
+    #     url = self._baseURL + "Authentication/AuthenticateToken?token=" + token
+    #     print(url)
+    #     response = requests.post(url, headers=self._headers)
+    #     result = response.json()
+    #     print(result)
+
+    def getContracts(self) -> list:
+        url = self._baseURL + "Contracts/ExportContracts"
+        return self.__getEntities(url)
+
+    def postContracts(self, contracts: list) -> bool:
+        url = self._baseURL + "Contracts/ImportContracts"
+        return self.__postEntities(url, contracts)
 
     class Domain:
         domainName: str
@@ -102,90 +107,16 @@ class eComply:
 
     def postDomainValues(self, domains: list[Domain]) -> bool:
         url = self._baseURL + "Catalog/ImportDomainNames"
-        response = requests.post(url, headers=self.getHeaders(), json=domains)
-
-        if not response.ok:
-            raise Exception(response)
-
-        response = requests.get(url, headers=self._headers)
-
-        if not response.ok:
-            raise Exception(response)
-
-        result = response.json()
-        if result["success"]:
-            print(
-                {
-                    "success": result["success"],
-                    "processedResults": result["data"]["processedResults"],
-                    "totalProcessed": result["data"]["totalProcessed"],
-                    "totalWithErrors": result["data"]["totalWithErrors"],
-                    "totalUpdated": result["data"]["totalUpdated"],
-                    "totalCreated": result["data"]["totalCreated"],
-                }
-            )
-        else:
-            print(
-                {
-                    "success": result["success"],
-                    "message": result["message"],
-                }
-            )
-
-        return result["success"]
+        return self.__postEntities(url, domains)
 
     def getWorkOrders(self) -> list:
         url = self._baseURL + "Contracts/ExportWorkOrders"
-        response = requests.get(url, headers=self.getHeaders())
-
-        if response.ok:
-            result = response.json()
-            return result["data"]
-        else:
-            print(response.json())
-            return []
+        return self.__getEntities(url)
 
     def postWorkOrders(self, workOrders: list):
         url = self._baseURL + "Contracts/ImportWorkOrders"
-        response = requests.post(url, headers=self.getHeaders(), json=workOrders)
-
-        if not response.ok:
-            raise Exception(response)
-
-        response = requests.get(url, headers=self._headers)
-
-        if not response.ok:
-            raise Exception(response)
-
-        result = response.json()
-        if result["success"]:
-            print(
-                {
-                    "success": result["success"],
-                    "processedResults": result["data"]["processedResults"],
-                    "totalProcessed": result["data"]["totalProcessed"],
-                    "totalWithErrors": result["data"]["totalWithErrors"],
-                    "totalUpdated": result["data"]["totalUpdated"],
-                    "totalCreated": result["data"]["totalCreated"],
-                }
-            )
-        else:
-            print(
-                {
-                    "success": result["success"],
-                    "message": result["message"],
-                }
-            )
-
-        return result["success"]
+        return self.__postEntities(url, workOrders)
 
     def getWorkOrderLineItems(self) -> list:
         url = self._baseURL + "Contracts/ExportWorkOrderLineItems"
-        response = requests.get(url, headers=self.getHeaders())
-
-        if response.ok:
-            result = response.json()
-            return result["data"]
-        else:
-            print(response.json())
-            return []
+        return self.__getEntities(url)
